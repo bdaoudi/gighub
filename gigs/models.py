e5f7a85841477841b3c9eb777137183a463b981a
benhalima Daoudi <benhalima.daoudi@gmail.com>
2019-06-07 16:46:40 +0100

    Create Gig model

--- gigs/models.py
+++ gigs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class Gig(models.Model):
+    artist = models.ForeignKey(User, on_delete=models.CASCADE)
+    dateTime = models.DateTimeField(default=timezone.now)
+    venue = models.CharField(max_length=200)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
